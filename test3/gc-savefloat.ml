812758fcea8e149426961c4617a7078fd286c55f
iwannatto <iwannatto@gmail.com>
2019-01-31 19:50:34 +0900

    Fix gc1 test

--- test3/gc-savefloat.ml
+++ test3/gc-savefloat.ml
@@ -5,7 +5,7 @@ prerr_int (f 100); prerr_byte 10;
 let t = (a, b) in
 do_nothing t;
 let int_a = Array.make 10 a in
-let float_a = Array.make 35 b in
+let float_a = Array.make 50 b in
 prerr_int int_a.(0); prerr_byte 10;
 prerr_float float_a.(0); prerr_byte 10;
 let c = read_float_0 () +. 1000.0 in
