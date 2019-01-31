eabc58707cb48028f37b47bc5167c9a243a95553
iwannatto <iwannatto@gmail.com>
2019-01-31 21:13:06 +0900

    Fix test

--- test3/gc-tuplealloc.ml
+++ test3/gc-tuplealloc.ml
@@ -2,13 +2,13 @@ let a = read_int_0 () + 1 in
 let b = read_float_0 () +. 10.0 in
 let rec f c = a + (int_of_float b) + c in
 prerr_int (f 100); prerr_byte 10;
-let t = (a, b) in
+let t = (a, (int_of_float b)) in
 do_nothing t;
-let int_a = Array.make 10 a in
+let int_a = Array.make 13 a in
 let float_a = Array.make 50 b in
 prerr_int int_a.(0); prerr_byte 10;
 prerr_float float_a.(0); prerr_byte 10;
 let tt = (a, a) in
-prerr_int a; prerr_byte 10;
+do_nothing tt;
 let (aa, aaa) = tt in
 prerr_int (f aa); prerr_byte 10
